f5a60df4a663fd98a8ef917c926b7e8b0dcc1d9d
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2022-08-31 21:01:55 +0200

    Wrote useApiService

--- hooks/useApiService.js
+++ hooks/useApiService.js
@@ -18,6 +18,7 @@ export default function useApiService() {
             return await request.json()
         } else if (request.status === 401) {
             logout()
+            throw new Error(`Unauthorized`)
         } else {
             const message = `Server returned the following status: ${request.status} while fetching sourcers`
             console.error(message);
@@ -33,6 +34,7 @@ export default function useApiService() {
             return await request.json()
         } else if (request.status === 401) {
             logout()
+            throw new Error(`Unauthorized`)
         } else {
             const message = `Server returned the following status: ${request.status} while fetching charts-metadata`
             console.error(message);
@@ -49,6 +51,7 @@ export default function useApiService() {
                     return res.json()
                 } else if (request.status === 401) {
                     logout()
+                    throw new Error(`Unauthorized`)
                 }
                 else {
                     throw new Error(`Server returned non 200 response: '${res.status}'`)
@@ -56,21 +59,139 @@ export default function useApiService() {
             })
     }, [authHeaders, logout])
 
+    const createDashboard = useCallback(async (dashboard) => {
+        return fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/dashboard`, {
+            method: 'POST',
+            headers: authHeaders.append('Content-Type', 'application/json'),
+            body: JSON.stringify(dashboard)
+        })
+            .then(async res => {
+                if (request.status === 401) {
+                    logout()
+                    throw new Error(`Unauthorized`)
+                }
+                else if (res.status !== 201) throw new Error(`Server returned non 201 response: '${res.status}'`)
+                const dashboard = await res.json()
+                return dashboard
+            })
+    }, [authHeaders, logout])
+
     const updateDashboard = useCallback(async (dashboard) => {
         return fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/dashboard`, {
             method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
+            headers: authHeaders.append('Content-Type', 'application/json'),
             body: JSON.stringify(dashboard)
         }).then(async res => {
             if (request.status === 401) {
                 logout()
+                throw new Error(`Unauthorized`)
             }
             else if (res.status !== 200) throw new Error(`Server returned non 200 response: '${res.status}'`)
             const returnedDashboard = await res.json()
             return returnedDashboard
         })
+    }, [authHeaders, logout])
+
+    const getChartData = useCallback(async (dashboardId) => {
+        return fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/dashboard/${dashboardId}/chart-data`, {
+            headers: authHeaders
+        })
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json()
+                }
+                else if (request.status === 401) {
+                    logout()
+                    throw new Error(`Unauthorized`)
+                }
+                else {
+                    throw new Error(`Server returned non 200 response: '${res.status}'`)
+                }
+            })
+    }, [authHeaders, logout])
+
+    const getChart = useCallback(async (dashboardId, chartId) => {
+        return fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/dashboard/${dashboardId}/charts/${chartId}`, {
+            headers: authHeaders
+        })
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json()
+                }
+                else {
+                    throw new Error(`Server returned non 200 response: '${res.status}'`)
+                }
+            })
+    }, [authHeaders, logout])
+
+    const addChart = useCallback(async (dashboardId, chart) => {
+        return fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/dashboard/${dashboardId}/charts`, {
+            method: 'POST',
+            headers: authHeaders.append('Content-Type', 'application/json'),
+            body: JSON.stringify(chart)
+        })
+            .then(async res => {
+                if (request.status === 401) {
+                    logout()
+                    throw new Error(`Unauthorized`)
+                }
+                if (res.status !== 201) throw new Error(`Server returned non 201 response: '${res.status}'`)
+                const chart = await res.json()
+                return chart
+            })
+    }, [authHeaders, logout])
+
+    const updateChart = useCallback(async (dashboardId, chart) => {
+        return fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/dashboard/${dashboardId}/charts`, {
+            method: 'PUT',
+            headers: authHeaders.append('Content-Type', 'application/json'),
+            body: JSON.stringify(chart)
+        })
+            .then(async res => {
+                if (request.status === 401) {
+                    logout()
+                    throw new Error(`Unauthorized`)
+                }
+                if (res.status !== 200) throw new Error(`Server returned non 200 response: '${res.status}'`)
+                const chart = await res.json()
+                return chart
+            })
+    }, [authHeaders, logout])
+
+    const searchDashboards = useCallback(async (term, size, from) => {
+        const params = new URLSearchParams()
+        params.append("size", size)
+        params.append("from", from)
+        if (term || term === '') params.append("term", term)
+
+        return fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/dashboard?${params.toString()}`, {
+            headers: authHeaders
+        })
+            .then(async res => {
+                if (res.status === 200) {
+                    return await res.json()
+                }
+                else if (request.status === 401) {
+                    logout()
+                    throw new Error(`Unauthorized`)
+                }
+                else {
+                    throw new Error(`Server returned non 200 response: '${res.status}'`)
+                }
+            })
+    }, [authHeaders, logout])
+
+    const updateVisualizationData = useCallback(async (dashboardId, visualizationDataObj) => {
+        return fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/dashboard/${dashboardId}/charts/visualizationData`, {
+            method: 'PUT',
+            headers: authHeaders.append('Content-Type', 'application/json'),
+            body: JSON.stringify(visualizationDataObj)
+        })
+            .then(async res => {
+                if (res.status !== 200) throw new Error(`Server returned non 200 response: '${res.status}'`)
+                const chart = await res.json()
+                return chart
+            })
     }, [authHeaders, logout])
 
 }
