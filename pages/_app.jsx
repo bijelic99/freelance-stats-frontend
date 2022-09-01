cd4e3144d53394707f4ec0d1e9ac15ada7131a38
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2022-09-01 20:37:45 +0200

    Created context providers

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,15 +4,21 @@ import '/node_modules/react-grid-layout/css/styles.css'
 import '/node_modules/react-resizable/css/styles.css'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { TokenContext } from '../contexts/tokenContext';
+import { UserContext } from '../contexts/userContext';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar/>
-      <ToastContainer/>
-      <main className='mt-2 mx-2'>
-        <Component {...pageProps} />
-      </main>
+      <TokenContext.Provider>
+        <UserContext.Provider>
+          <Navbar />
+          <ToastContainer />
+          <main className='mt-2 mx-2'>
+            <Component {...pageProps} />
+          </main>
+        </UserContext.Provider>
+      </TokenContext.Provider>
     </>
   )
 }
